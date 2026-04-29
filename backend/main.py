65ab8fce77c3bd031f446ed38fd06d02cad0679f
miichaelleon <260804927+miichaelleon@users.noreply.github.com>
2026-04-29 15:28:02 -0500

    final

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://project-p4lhh.vercel.app"],
+    allow_origins=["http://localhost:5173", "https://project-p4lhh.vercel.app", ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
